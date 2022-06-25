b2e6c84210a947c996307cd790f7528d7447588c
Gourav Mac <gouravofficial19@gmail.com>
2022-06-25 12:47:20 +0530

    added return value

--- retrofitLib/src/main/java/com/gourav/retrofitlib/BuildRetrofit.kt
+++ retrofitLib/src/main/java/com/gourav/retrofitlib/BuildRetrofit.kt
@@ -1,16 +1,15 @@
 package com.gourav.retrofitlib
 
-import android.content.Context
-import android.widget.Toast
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object BuildRetrofit {
 
     fun getRetrofitInstance(BASE_URL: String): Retrofit {
-        return Retrofit.Builder()
+        val retroClient = Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
+        return retroClient
     }
 }
