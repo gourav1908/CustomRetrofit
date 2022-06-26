91e734a3b8d08c135d03729123c877f3f9095bdf
Gourav Mac <gouravofficial19@gmail.com>
2022-06-26 13:00:22 +0530

    demo

---
+++ app/src/main/java/com/gourav/androidlibrary/api/APIService.kt
@@ -0,0 +1,23 @@
+package com.gourav.androidlibrary.api
+
+import com.google.gson.JsonElement
+import com.gourav.retrofitlib.model.ResponseModel
+import com.gourav.uselibrary.models.NewsModel
+import retrofit2.Call
+import retrofit2.Response
+import retrofit2.http.*
+
+interface APIService {
+    @GET("userProfile")
+    fun getAppointmentDetails(
+        @Query("email") email: String,
+        @Query("password") passwrod: String
+    ): ResponseModel
+
+    @GET("top-headlines")
+    suspend fun getHeadlines(
+        @Query("country") country: String,
+        @Query("category") category: String,
+        @Query("apiKey") apiKey: String
+    ): Response<JsonElement>
+}
